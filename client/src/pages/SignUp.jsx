127e5fb22652eb9155db32461f65c9a60d344525
Usman Ghori <usman.ghori1@icloud.com>
2025-01-29 02:09:35 -0800

    Add Sign up functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,8 +1,49 @@
-import { Button, Label, TextInput } from "flowbite-react";
-import React from "react";
-import { Link } from "react-router-dom";
+import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 const SignUp = () => {
+  
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+  
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+  };
+
+  const handleSubmit = async (e)=> {
+    e.preventDefault();
+
+    if(!formData.username || !formData.email || !formData.password){
+      return setErrorMessage('Please fill all the fields.')
+    }
+
+    try {
+      setLoading(true);
+      setErrorMessage(null);
+      const res = await fetch('/api/auth/signup', {
+        method: 'POST',
+        headers: {'Content-Type' : 'application/json'},
+        body: JSON.stringify(formData)
+      });
+
+      const data = await res.json()
+      if(data.success === false){
+        return setErrorMessage(data.message)
+      }
+      setLoading(false)
+      if(res.ok){
+        navigate('/sign-in')
+      }
+    } catch (error) {
+      setErrorMessage(error.message)
+      setLoading(false)
+    }
+  };
+  
   return (
     <div className="min-h-screen mt-20">
       <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
@@ -17,35 +58,67 @@ const SignUp = () => {
             </span>
           </Link>
           <p className="mt-5 text-sm">
-            This is a MERN Stack and Firebase project for a blog application. 
+            This is a MERN Stack and Firebase project for a blog application.
             This project is for my portfolio so I can show my work to clients.
           </p>
         </div>
 
         {/* right side */}
         <div className="flex-1">
-          <form className="flex flex-col gap-4">
+          <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
             <div className="">
-              <Label value="Your username"/>
-              <TextInput type="text" placeholder="Username" id="username"/>
+              <Label value="Your username" />
+              <TextInput
+                type="text"
+                placeholder="Username"
+                id="username"
+                onChange={handleChange}
+              />
             </div>
 
             <div className="">
-              <Label value="Your email"/>
-              <TextInput type="email" placeholder="Email" id="email"/>
+              <Label value="Your email" />
+              <TextInput
+                type="email"
+                placeholder="Email"
+                id="email"
+                onChange={handleChange}
+              />
             </div>
 
             <div className="">
-              <Label value="Your password"/>
-              <TextInput type="password" placeholder="Password" id="password"/>
+              <Label value="Your password" />
+              <TextInput
+                type="password"
+                placeholder="Password"
+                id="password"
+                onChange={handleChange}
+              />
             </div>
-            <Button gradientDuoTone="purpleToPink" type="submit">Sign Up</Button>
+            <Button gradientDuoTone="purpleToPink" type="submit" disabled={loading}>
+              {
+                loading ? (
+                  <>
+                  <Spinner size="sm"/>
+                  <span className="pl-3">Loading... </span></>
+                ) : "Sign Up"
+              }
+            </Button>
           </form>
 
           <div className="flex gap-2 text-sm mt-5">
             <span>Have an account?</span>
-            <Link to='/sign-in' className="text-blue-500 font-semibold">Sign In</Link>
+            <Link to="/sign-in" className="text-blue-500 font-semibold">
+              Sign In
+            </Link>
           </div>
+          {
+            errorMessage && (
+              <Alert className="mt-5" color="failure">
+                {errorMessage}
+              </Alert>
+            )
+          }
         </div>
       </div>
     </div>
