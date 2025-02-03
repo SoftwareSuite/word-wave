eecaf727c282559bec4193ee1607f616c8ee3891
Usman Ghori <usman.ghori1@icloud.com>
2025-02-03 01:13:38 -0800

    Complete update User API route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv'
+import cookieParser from 'cookie-parser';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 
@@ -16,7 +17,8 @@ mongoose.connect(process.env.MONGO)
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
+app.use(cookieParser());
 
 app.listen(3000, ()=> {
     console.log("server is working on port 3000!!")
