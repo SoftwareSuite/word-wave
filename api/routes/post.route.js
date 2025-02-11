0de7d10d8fc0b73b455f404e3d6cf7a21290a937
Usman Ghori <usman.ghori1@icloud.com>
2025-02-11 11:10:34 -0800

    add update post functionality and API route

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,11 +1,12 @@
 import express from'express';
 import { verifyToken } from '../utils/verifyUser.js';
-import { create, deletePost, getposts } from '../controllers/post.controller.js';
+import { create, deletePost, getposts, updatePost } from '../controllers/post.controller.js';
 
 const postRouter = express.Router();
 
 postRouter.post('/create', verifyToken, create)
 postRouter.get('/getposts', getposts)
 postRouter.delete('/deletepost/:postId/:userId', verifyToken, deletePost)
+postRouter.put('/updatepost/:postId/:userId', verifyToken, updatePost)
 
 export default postRouter;
