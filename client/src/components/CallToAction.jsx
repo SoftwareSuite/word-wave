c604b34ce4e9ae31d808115a5fa70b59b819e70f
Usman Ghori <usman.ghori1@icloud.com>
2025-02-15 11:18:41 -0800

    removing errors in Cal to action.

--- client/src/components/CallToAction.jsx
+++ client/src/components/CallToAction.jsx
@@ -11,13 +11,13 @@ export default function CallToAction() {
                 Checkout these resources with 100 JavaScript Projects
             </p>
             <Button gradientDuoTone='purpleToPink' className='rounded-tl-xl rounded-bl-none'>
-                <a href="https://www.100jsprojects.com" target='_blank' rel='noopener noreferrer'>
-                    100 JavaScript Projects
+                <a href="https://www.usmanghori.com" target='_blank' rel='noopener noreferrer'>
+                    Hire, Best MERN Stack Developer
                 </a>
             </Button>
         </div>
         <div className="p-7 flex-1">
-            <img src="https://bairesdev.mo.cloudinary.net/blog/2023/08/What-Is-JavaScript-Used-For.jpg" />
+            <img src="https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/gigs/272503033/original/4555220aecad098decb806361e6094b4ca17bc11/django-and-python-web-page.jpg" />
         </div>
     </div>
   )
