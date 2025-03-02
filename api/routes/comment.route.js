5289ab77fe5d091b6bbd422ecf1a0152968ff6a7
Usman Ghori <usman.ghori1@icloud.com>
2025-03-02 14:27:28 -0800

    create like and unlike comment Api route and add functionality to frontend

--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -1,10 +1,10 @@
 import express from 'express';
-import { createComment, getPostComments } from '../controllers/comment.controller.js';
+import { createComment, getPostComments, likeComment } from '../controllers/comment.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const commentRouter = express.Router()
 
 commentRouter.post('/create', verifyToken, createComment)
 commentRouter.get('/getpostcomments/:postId' , getPostComments)
-
+commentRouter.put('/likecomment/:commentId', verifyToken, likeComment)
 export default commentRouter
